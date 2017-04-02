df24f310a9aef16d2034705c327129d2394d623c
Naughtron <chris.naughton@gmail.com>
2017-04-02 13:01:54 -0400

    update to reference test

--- reference.py
+++ reference.py
@@ -1,104 +1,94 @@
+# -*- coding: UTF-8 -*-
 # imports
-import requests
 import unittest
+import unicodedata
 import sys
+import uuid
+import random
+import string
+import time
+from faker import Faker
+from nameparser import HumanName
 from selenium import webdriver
 # imports to run Selenium headless
 from pyvirtualdisplay import Display
 
-# unit test class
-'''setup'''
-class TestHelloWorld(unittest.TestCase):
-	server_address = ''
-
-	def setUp(self):
-		self.server_address = "localhost:5000"
-
-	def test_HelloWorld(self):
-		url_template = 'http://%s'
-		url = url_template %(self.server_address)
-
-		# verify url
-		#print url
-
-		# make the request
-		resp = requests.get(url)
-
-		# import pdb; pdb.set_trace()
-
-		# assert response
-		self.assertEqual(resp.status_code, 200)
-		# assert text coming back
-		self.assertEqual(resp.text, u'Hello World!')
-
-# unit test for /mod2
-class TestModTwoPageContents(unittest.TestCase):
-	server_address = ''
-
-	def setUp(self):
-		self.server_address = "localhost:5000"
-
-	def test_mod2(self):
-		url_template = 'http://%s/mod2'
-		url = url_template %(self.server_address)
-
-		#print url
-
-		resp = requests.get(url)
-		#import pdb; pdb.set_trace()
-
-		self.assertEqual(resp.status_code, 200)
-		# this is just a bad assert self.assertEqual(resp.text, u'<!--this is the first template \n<html>\n <head>\n \t<title>Home - microblog</title>\n </head>\n <body>\n \t<h1>Hello, Naughtron!</h1>\n </body>\n</html> -->\n\n<!-- user Jinja to set control statements in a template-->\n<!--<html>\n <head>\n \t \n \t<title>Home - microblog</title>\n \t\n </head>\n <body>\n \t<h1>Greetings, Naughtron!</h1>\n \t \n \t<p>Jeff says: <b>Be the bomb you throw!</b></p> \n \t \n \t<p>Connor says: <b>I am a real life lore nerd</b></p> \n \t \n </body>\n</html>-->\n\n<!--inherit from the base template-->\n<!--this is the base template that allows other templates to inherit core features-->\n<!-- the index.html will inherit from this template-->\n<html>\n <head>\n \t\n \t<title>Home - microblog</title>\n \t\n </head>\n <body>\n \t<div>Microblog: <a href="/index">Home</a></div>\n \t<hr>\n \t\n<h1>Greetings, Naughtron!</h1>\n\n<div><p>Jeff says: <b>Be the bomb you throw!</b></p></div>\n\n<div><p>Connor says: <b>I am a real life lore nerd</b></p></div>\n\n\n </body>\n</html>')
-
-# unit tests for mod 3
-class TestLoginIsUp(unittest.TestCase):
-	server_address = ''
-
-	def setUp(self):
-		self.server_address = "localhost:5000"
-
-	def test_login_page_is_there(self):
-		url_template = 'http://%s/login'
-		url = url_template %(self.server_address)
-
-		#print url
-		resp = requests.get(url)
-
-		# assert
-		self.assertEqual(resp.status_code, 200)
-
-
-# selenium test for /mod2
+# selenium test for /create_account
 # verify: posts, and page name
-class TestMod2Selenium(unittest.TestCase):
+class verify_home_title(unittest.TestCase):
 	def setUp(self):
-		self.display = Display(visible=0, size=(800, 600))
-		self.display.start()
-		self.driver = webdriver.Firefox()
+		#self.display = Display(visible=0, size=(800, 600))
+		#self.display.start()
+		self.driver = webdriver.Chrome()
+		self.server = "https://www.oreilly.com/"
 
 	# simple verification of page title
 	def test_validate_page_elements(self):
 		driver = self.driver
-		driver.get("http://127.0.0.1:5000/mod2")
-		self.assertIn("Home - microblog", driver.title)
+		server = self.server
+		title_assert = "OReilly Media - Technology and business training, knowledge, and insight, delivered by experts and innovators"
+		driver.get(server)
+		# strip unicode from title
+		title = unicodedata.normalize('NFKD', driver.title).encode('ascii', 'ignore')
+		# assert the title is correct
+		self.assertIn(title_assert, title)
+		
 	def tearDown(self):
 		self.driver.close()
-		self.display.stop()
+		#self.display.stop()
 
-class TestMod2HomeClick(unittest.TestCase):
+class create_new_account(unittest.TestCase):
 	# test setup
 	def setUp(self):
-		self.display = Display(visible=0, size=(800, 600))
-		self.display.start()
-		self.driver = webdriver.Firefox()
-	# test
+		#self.display = Display(visible=0, size=(800, 600))
+		#self.display.start()
+		# create faker
+		fake = Faker()
+		# create rand string
+		rand_string = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(10)])
+		# create user data: 
+		u_name = fake.name()
+		spl_name = HumanName(u_name)
+		f_uname = spl_name.first
+		l_uname = spl_name.last
+		usr_email = f_uname + l_uname + "@notrealemail.com"
+		disp_name = f_uname + l_uname + rand_string
+		u_password = "4lw4ysb3" + rand_string
+		self.driver = webdriver.Chrome()
+		self.server = "https://www.oreilly.com/"
+	# test create new account
 	def test_validate_click_home(self):
 		driver = self.driver
-		driver.get("http://127.0.0.1:5000/mod2")
-		# find the home line
-		home_link = driver.find_element_by_css_selector('a[href="/index"]')
-		home_link.click()
-		#assertEqual(driver.title,"Home - microblog")
-
+		server = self.server
+		driver.get(server)
+		# get to the create page
+		r_logout = driver.find_element_by_id('log-out')
+		r_logout.click()
+		time.sleep(2)
+		# click on create new account
+		r_create_acct = driver.find_element_by_id('capture_signIn_traditionalSignIn_createButton')
+		r_create_acct.click()
+		# fill out new user form
+		r_first_name = driver.find_element_by_id('capture_traditionalRegistration_traditionalRegistration_firstName')
+		r_first_name.sendKeys(f_uname)
+		
+		r_last_name = driver.find_element_by_id('capture_traditionalRegistration_form_item_traditionalRegistration_lastName')
+		r_last_name.sendKeys(l_uname)
+		
+		r_display_name = driver.find_element_by_id('capture_traditionalRegistration_form_item_traditionalRegistration_displayName')
+		r_display_name.sendKeys(disp_name)
+		
+		r_email = driver.find_element_by_id('capture_traditionalRegistration_form_item_traditionalRegistration_emailAddress')
+		r_email.sendKeys(usr_email)
+		
+		r_password_og = driver.find_element_by_id('capture_traditionalRegistration_form_item_traditionalRegistration_password')
+		r_password_og.sendKeys(u_password)
+		
+		r_password_cfm = driver.find_element_by_id('capture_traditionalRegistration_form_item_traditionalRegistration_passwordConfirm')
+		r_password_cfm.sendKeys(u_password)		
+		
+		
+		
+		
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
